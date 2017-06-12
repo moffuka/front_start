b3fb0c4f50dd623e03d9ead797fff4567850d28a
Volode <vladimir1855@gmail.com>
2017-06-12 12:42:09 +0300

    some fixes sprites support

--- frontend/web/src/js/controllers/socials.js
+++ frontend/web/src/js/controllers/socials.js
@@ -3,8 +3,7 @@ var socials = new (BaseController.extend({
     init: function(){
         var that = this;
 
-        $(document).on('click', '.b-footer_social', function(e){ that.sharingClick(this); });
-        $(document).on('click', '.b-social', function(e){ that.sharingOtherClick(this); });
+        $(document).on('click', '.b-footer_social', function(e){ that.sharingClick(this, {}); });
     },
 
 
@@ -35,115 +34,74 @@ var socials = new (BaseController.extend({
 
     /********************************************** SHARINGS ********************************************/
 
-    sharingClick: function(elem, callback) {
+    sharingClick: function (elem, _meta, callback) {
         var that = this;
+        callback = callback || function () {
+            };
         var soc_type = $(elem).attr('data-soc-type') || '';
 
+        var meta = $.extend({
+            title: 'Veb Idea',
+            descr: 'Veb Idea Descr',
+            link : shareUrl,
+            photo: shareUrl + '/img/socials/share.jpg',
+        }, _meta);
+
+        if (meta.photo.indexOf('http') == -1) {
+            meta.photo = shareUrl + meta.photo;
+        }
+
         if (soc_type != '') {
-            var share_title = encodeURIComponent('Site Title');
-            var share_descr = encodeURIComponent('Site Descr');
-            var share_link = encodeURIComponent(baseUrl);
-            var share_image = encodeURIComponent(baseUrl + '/img/socials/share.jpg');
+            var share_title = encodeURIComponent(meta.title);
+            var share_descr = encodeURIComponent(meta.descr);
+            var share_link = encodeURIComponent(meta.link);
+            var share_image = encodeURIComponent(meta.photo);
 
             var link = '';
             switch (soc_type) {
                 case 'fb':
-                    link = 'https://www.facebook.com/sharer/sharer.php?u=' + share_link;
+                    link = 'https://www.facebook.com/dialog/feed?app_id=' + fb_app_id + '&display=popup&name=' + share_title + '&description=' + share_descr + '&picture=' + share_image + '&link=' + share_link + '&redirect_uri=' + share_link;
                     break;
                 case 'vk':
-                    link = 'http://vkontakte.ru/share.php?url='+share_link+'&image='+share_image+'&title='+share_title+'&description='+share_descr;
+                    link = 'http://vkontakte.ru/share.php?url=' + share_link + '&image=' + share_image + '&title=' + share_title + '&description=' + share_descr;
                     break;
                 case 'ok':
-                    link = 'http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1&st._surl='+share_link;
+                    link = 'https://connect.ok.ru/dk?cmd=WidgetSharePreview&st.cmd=WidgetSharePreview&st._aid=ExternalShareWidget_SharePreview&st.imageUrl=' + share_image + '&st.description=' + share_descr + '&st.shareUrl=' + share_link + '&st.title=' + share_title;
                     break;
                 case 'tw':
-                    link = 'https://twitter.com/intent/tweet?original_referer='+share_link+'&tw_p=tweetbutton&url='+share_link+'&text='+share_title;
+                    link = 'https://twitter.com/intent/tweet?original_referer=' + share_link + '&tw_p=tweetbutton&url=' + share_link + '&text=' + share_title;
                     break;
             }
 
             var share_window = window.open(link, '_blank', that.getWindowFeatures());
             if (share_window) {
-                var uri_regex = new RegExp(baseUrl);
+                var uri_regex = new RegExp(shareUrl);
 
                 var watch_timer = setInterval(function () {
                     try {
                         if (share_window.closed) {
                             clearInterval(watch_timer);
 
-                            if (typeof callback == 'function') { callback(); }
+                            callback();
                             return;
                         }
                         if (uri_regex.test(share_window.location)) {
                             clearInterval(watch_timer);
 
                             share_window.close();
-                            if (typeof callback == 'function') { callback(); }
+                            callback();
                         }
-                    } catch (e) { }
+                    } catch (e) {
+                    }
                 }, 200);
             } else {
                 location.href = link;
-                setTimeout(function(){
-                    if (typeof callback == 'function') { callback(); }
+                setTimeout(function () {
+                    callback();
                 }, 500);
             }
         }
     },
-
-    sharingOtherClick: function(elem, callback) {
-        var that = this;
-        var soc_type = $(elem).attr('data-soc-type') || '';
-
-        if (soc_type != '') {
-            var share_title = encodeURIComponent('Site Title');
-            var share_descr = encodeURIComponent('Site Descr');
-            var share_link = encodeURIComponent(baseUrl);
-            var share_image = encodeURIComponent(baseUrl + '/img/socials/share.jpg');
-
-            var link = '';
-            switch (soc_type) {
-                case 'fb':
-                    link = 'https://www.facebook.com/dialog/feed?app_id='+fb_app_id+'&display=popup&name='+share_title+'&description='+share_descr+'&picture='+share_image+'&link='+share_link+'&redirect_uri='+share_link;
-                    break;
-                case 'vk':
-                    link = 'http://vkontakte.ru/share.php?url='+share_link+'&image='+share_image+'&title='+share_title+'&description='+share_descr;
-                    break;
-                case 'ok':
-                    link = 'http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1&st._surl='+share_link;
-                    break;
-                case 'tw':
-                    link = 'https://twitter.com/intent/tweet?original_referer='+share_link+'&tw_p=tweetbutton&url='+share_link+'&text='+share_title;
-                    break;
-            }
-
-            var share_window = window.open(link, '_blank', that.getWindowFeatures());
-            if (share_window) {
-                var uri_regex = new RegExp(baseUrl);
-
-                var watch_timer = setInterval(function () {
-                    try {
-                        if (share_window.closed) {
-                            clearInterval(watch_timer);
-
-                            if (typeof callback == 'function') { callback(); }
-                            return;
-                        }
-                        if (uri_regex.test(share_window.location)) {
-                            clearInterval(watch_timer);
-
-                            share_window.close();
-                            if (typeof callback == 'function') { callback(); }
-                        }
-                    } catch (e) { }
-                }, 200);
-            } else {
-                location.href = link;
-                setTimeout(function(){
-                    if (typeof callback == 'function') { callback(); }
-                }, 500);
-            }
-        }
-    }
 
 
 
